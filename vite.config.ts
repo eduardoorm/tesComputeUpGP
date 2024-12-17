72b4398f994fe3ee017cc8bd388702eb1a5c7b41
José Eduardo Ormeño Meneses <eduardoorm79@gmail.com>
2024-12-17 17:13:44 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Compute-Up/web-computeup.git',
+  base: '/web-computeup/',
 
 })
