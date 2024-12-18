07faefa331ad842300b369ec831a16e0ba92fc2e
José Eduardo Ormeño Meneses <eduardoorm79@gmail.com>
2024-12-18 17:51:52 -0500

    base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/tesComputeUpGP/', // Reemplaza 'nombre-del-repositorio' con el nombre de tu repositorio
   plugins: [react()],
   build: {
     outDir: 'dist',
